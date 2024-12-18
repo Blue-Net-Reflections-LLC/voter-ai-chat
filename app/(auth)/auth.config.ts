f2644635ff53392a30ae32a101a2ce8aaa2ee382
dukesvrtech <horace.reid@bluenetreflections.com>
2024-12-18 12:33:47 -0500

    fixing build

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -13,22 +13,22 @@ export const authConfig = {
 
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      return true;
-      // const isLoggedIn = !!auth?.user;
-			// const isHomePage = nextUrl.pathname === '/'
-			// 	const isOnChat = nextUrl.pathname.startsWith('/chat');
-      // const isOnRegister = nextUrl.pathname.startsWith('/register');
-      // const isOnLogin = nextUrl.pathname.startsWith('/login');
-			// if (isOnChat) {
-			// 	if (isLoggedIn) return true;
-			// 	return false; // Redirect unauthenticated users to login page
-			// }
-
-      // if (isLoggedIn && (isOnLogin || isOnRegister || isHomePage)) {
-      //   return Response.redirect(new URL('/chat', nextUrl as unknown as URL));
-      // }
-
       // return true;
+      const isLoggedIn = !!auth?.user;
+			const isHomePage = nextUrl.pathname === '/'
+				const isOnChat = nextUrl.pathname.startsWith('/chat');
+      const isOnRegister = nextUrl.pathname.startsWith('/register');
+      const isOnLogin = nextUrl.pathname.startsWith('/login');
+			if (isOnChat) {
+				if (isLoggedIn) return true;
+				return false; // Redirect unauthenticated users to login page
+			}
+
+      if (isLoggedIn && (isOnLogin || isOnRegister || isHomePage)) {
+        return Response.redirect(new URL('/chat', nextUrl as unknown as URL));
+      }
+
+      return true;
     },
   },
 } satisfies NextAuthConfig;
