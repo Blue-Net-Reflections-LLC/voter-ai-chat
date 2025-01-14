36b73a608c81bb71ce957af1834fa0fabcb25f7e
dukesvrtech <horace.reid@bluenetreflections.com>
2025-01-14 15:45:43 -0500

    Working build

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
 import NextAuth from 'next-auth';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 import { authConfig } from '@/app/(auth)/auth.config';
 
 export default NextAuth(authConfig).auth;
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/register') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+}
 
 export const config = {
   matcher: ['/', '/:id', '/api/:path*', '/login', '/register', '/voter-management'],
